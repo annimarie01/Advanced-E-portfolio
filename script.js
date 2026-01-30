bdf114a146fb24f1e96ea59af4306c6b02daeb90
annimarie01 <anni.thekindfairy@gmail.com>
2026-01-30 10:27:25 -0700

    dark mode

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// template_omiztt7
-// service_9gdnjht
-// sha_EqBtAvcUuOzyH
+let isModalOpen = false;
+let contrastToggle = false;
 
-function contact()
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    // toggle modal
+    document.body.classList += " modal--open";
+
+}
